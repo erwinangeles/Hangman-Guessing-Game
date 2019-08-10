63375b141a59d7a9a7207d5b733fae42ce547475
Erwin Angeles <erwin@eang.media>
2019-08-10 17:21:31 -0500

    adding music files/images/and updating win/loss count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,184 @@
 const wordBank = ['BISON','BLANCA','CHUNLI','EHONDA','KEN','RYU','SAGAT','SAKURA'];
-let userGuesses = [];
-let wordGuessed = [];
-let randomFighter = Math.floor(Math.random() * (wordBank.length));
+let totalGuesses = 9;
+let userGuesses = []; // letters the user guessed
+let wordGuessed = []; // This will be the word we actually build to match the current word
+let randomFighter = null;
+let guessesLeft = 0;
 let wins = 0;
 let losses = 0;
-let winStatus = false;
-let lossStatus = false;
+let gameover = false;
 
+const koSound = new Audio('./assets/mp3/ko-sound.mp3');
+const youLose = new Audio('./assets/mp3/lose.mp3');
+const perfect = new Audio('./assets/mp3/perfect.mp3');
+const final = new Audio('./assets/mp3/final.mp3');
 
+const coinSound = new Audio('./assets/mp3/coin_1.mp3');
+const startSound = new Audio('./assets/mp3/playagain.wav');
+const bgMusic = new Audio("./assets/mp3/music.mp3");
 
 document.addEventListener("DOMContentLoaded", function(){
-   pickrandomFighter();
-   let currentWordDiv = document.querySelector('#currentWord')
-    currentWordDiv.textContent = wordGuessed;
+    startGame();
 });
 
+function startGame(){
+    pickrandomFighter();
+    
+}
 function pickrandomFighter(){
-    let myDiv = document.querySelector("#hint");
-    let randomFighter = Math.floor(Math.random() * (wordBank.length));
+    wordGuessed = [];
+    userGuesses = [];
+    guessesLeft = totalGuesses;
+    randomFighter = Math.floor(Math.random() * (wordBank.length));
+    let divHint = document.querySelector("#hint");
+   
+    // let randomFighter = Math.floor(Math.random() * (wordBank.length));
     if(randomFighter == 0){
-        myDiv.innerHTML = "<img src='assets/images/bison.png' width='100%'/>";
+        divHint.innerHTML = "<img src='assets/images/bison.png' height='400px'style='padding:20px'/>";
     }else if(randomFighter == 1){
-        myDiv.innerHTML = "<img src='assets/images/blanca.png' width='100%'/>";
+        divHint.innerHTML = "<img src='assets/images/blanca.png' height='300px'style='padding:20px'/>";
     }else if(randomFighter == 2){
-        myDiv.innerHTML = "<img src='assets/images/chunli.png' width='100%'/>";
+        divHint.innerHTML = "<img src='assets/images/chunli.png' height='300px' style='padding:20px'/>";
     }else if(randomFighter == 3){
-        myDiv.innerHTML = "<img src='assets/images/ehonda.png' width='100%'/>";
+        divHint.innerHTML = "<img src='assets/images/ehonda.png' height='300px'style='padding:20px'/>";
     }else if(randomFighter == 4){
-        myDiv.innerHTML = "<img src='assets/images/ken.png' width='100%'/>";
+        divHint.innerHTML = "<img src='assets/images/ken.png'height='300px' style='padding:20px'/>";
     }else if(randomFighter == 5){
-        myDiv.innerHTML = "<img src='assets/images/ryu.png' width='100%'/>";
+        divHint.innerHTML = "<img src='assets/images/ryu.png' height='300px' style='padding:20px'/>";
     }else if(randomFighter == 6){
-        myDiv.innerHTML = "<img src='assets/images/sagat.png' width='100%'/>";
+        divHint.innerHTML = "<img src='assets/images/sagat.png'  height='300px' style='padding:20px'/>";
     }else if(randomFighter == 7){
-        myDiv.innerHTML = "<img src='assets/images/sakura.png' width='100%'/>";
+        divHint.innerHTML = "<img src='assets/images/sakura.png' height='300px' style='padding:20px'/>";
     }
-    for (var i = 0; i < wordBank[randomFighter].length; i++) {
+    for (let i = 0; i < wordBank[randomFighter].length; i++) {
+      
         wordGuessed.push("_");
     }   
+    console.log(wordBank[randomFighter]);
+    let currentWordDiv = document.querySelector('#currentWord')
+    currentWordDiv.textContent = wordGuessed; //sets the word guessed
 }
 
 
 
 
+
+//guessing
+function makeGuess(letter) {
+    if (guessesLeft > 0) {
+        // Make sure we didn't use this letter
+        if (userGuesses.indexOf(letter) === -1) {
+            userGuesses.push(letter);
+            evaluateGuess(letter);
+        }
+    }
+};
+
+//compare letters entered to the character you're trying to guess
+function evaluateGuess(letter) {
+    let positions = [];
+
+    for (let i = 0; i < wordBank[randomFighter].length; i++) {
+        if(wordBank[randomFighter][i] === letter) {
+            positions.push(i); //pushes letter to spot
+            bgMusic.volume = 0.2;
+            coinSound.play();
+            bgMusic.volume = 1;
+        }
+    }
+
+    if (positions.length <= 0) {
+        guessesLeft--;
+    } else {
+        for(let i = 0; i < positions.length; i++) {
+            wordGuessed[positions[i]] = letter;
+        }
+    }
+};
+
+
+//  Updates the display on the HTML Page
+function updateGame() {
+    document.getElementById("winsDiv").innerText = wins;
+    document.getElementById("lossesDiv").innerText = losses;
+
+    let guessingWordText = "";
+    for (let i = 0; i < wordGuessed.length; i++) {
+        guessingWordText += wordGuessed[i];
+    }
+
+    //update guesses, word, and letters entered
+    document.getElementById("currentWord").innerText = guessingWordText;
+    document.getElementById("guessesLeft").innerText = guessesLeft;
+    document.getElementById("userGuesses").innerText = userGuesses;
+};
+
+//check if all letters have been entered.
+function checkWin() {
+    if(wordGuessed.indexOf("_") === -1) {
+        wins++;
+        gameover = true;
+        perfect.play();
+        //shows new win/loss score
+        document.getElementById("winsDiv").innerText = wins;
+        document.getElementById("lossesDiv").innerText = losses;
+        //resets word guesses
+        userGuesses = [];
+        guessesLeft = 9;
+        document.getElementById("guessesLeft").innerText = guessesLeft;
+        document.getElementById("userGuesses").innerText = userGuesses;
+    }
+};
+
+//check if the user is out of guesses
+function checkLoss()
+{
+    if(guessesLeft <= 0) {
+        losses++;
+        gameover = true;
+        bgMusic.pause();
+        youLose.play();
+
+         //shows new win/loss score
+        document.getElementById("winsDiv").innerText = wins;
+        document.getElementById("lossesDiv").innerText = losses;
+    
+    }
+}
+
+function playAgain()
+{
+    let totalGuesses = 9;
+    let userGuesses = []; // letters the user guessed
+    let wordGuessed = []; // This will be the word we actually build to match the current word
+    let gameover = false;
+    startSound.play();
+    startGame();
+
+}
+
+function playMusic(){
+    bgMusic.loop = true
+    bgMusic.play();
+}
+function pauseMusic(){
+    bgMusic.pause();
+}
+
+
 document.onkeydown = function logKey(e) {
+    if(gameover) {
+        startGame();
+        gameover = false;
+    } else {
     if(event.keyCode >= 65 && event.keyCode <= 90) { //makes sure key is a-z
-    userGuesses.push(`${e.key.toUpperCase()}`)
-    let userGuessesDiv = document.querySelector('#guesses')
-    userGuessesDiv.textContent = userGuesses;
+    // userGuesses.push(`${e.key.toUpperCase()}`)
+    // let userGuessesDiv = document.querySelector('#guesses')
+    // userGuessesDiv.textContent = userGuesses;
+    makeGuess(e.key.toUpperCase());
+    updateGame()
+    checkWin();
+    checkLoss();
+    }
     }
 }
